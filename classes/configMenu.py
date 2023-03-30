61b272e7856855f017b2bd9e93d723a9aa33ac61
Sam Hartley <121134897+samh366@users.noreply.github.com>
2023-03-30 16:39:35 +0100

    small changes

--- classes/configMenu.py
+++ classes/configMenu.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Frame
+from tkinter import Tk, Label, Frame, Entry
 from tkinter import ttk
 
 class Menu:
@@ -19,10 +19,35 @@ class Menu:
     def addFrame(self) -> Frame:
         """Adds a frame and all its appropriate elements"""
         frame = Frame(self.win, height=100, width=550, borderwidth=2, relief="sunken")
-        # Title
-        Label(frame, text="Object " + str(len(self.frames)+1), font=("Calibri 12")).grid(column=0, row=0)
-
         frame.pack()
+        # Title
+        Frame(frame, height=1, width=200).grid(column=0, row=0)
+        Label(frame, text="Object " + str(len(self.frames)+1), font=("Calibri 12")).grid(column=1, row=0)
+        # Mass
+        leftFrame = Frame(frame)
+        leftFrame.grid(column=0, row=1)
+        Label(leftFrame, text="Mass ", font=("Calibri 11")).grid(column=0, row=0)
+        Entry(leftFrame, font=("Calibri 11")).grid(column=1, row=0)
+        # Pos
+        Label(leftFrame, text="Position x", font=("Calibri 11")).grid(column=0, row=1)
+        Entry(leftFrame, font=("Calibri 11")).grid(column=1, row=1)
+        Label(leftFrame, text="Position y", font=("Calibri 11")).grid(column=0, row=2)
+        Entry(leftFrame, font=("Calibri 11")).grid(column=1, row=2)
+        # Velocity 
+        rightFrame = Frame(frame)
+        rightFrame.grid(column=2, row=1)
+        Label(rightFrame, text="Position x", font=("Calibri 11")).grid(column=0, row=0)
+        Entry(rightFrame, font=("Calibri 11")).grid(column=1, row=0)
+        Label(rightFrame, text="Position y", font=("Calibri 11")).grid(column=0, row=1)
+        Entry(rightFrame, font=("Calibri 11")).grid(column=1, row=1)
+        # Colour
+        Label(rightFrame, text="Colour R", font=("Calibri 11")).grid(column=0, row=2)
+        Entry(rightFrame, font=("Calibri 11"), width=3).grid(column=1, row=2, sticky="W")
+        Label(rightFrame, text="Colour G", font=("Calibri 11")).grid(column=0, row=3)
+        Entry(rightFrame, font=("Calibri 11"), width=3).grid(column=1, row=3, sticky="W")
+        Label(rightFrame, text="Colour B", font=("Calibri 11")).grid(column=0, row=4)
+        Entry(rightFrame, font=("Calibri 11"), width=3).grid(column=1, row=4, sticky="W")
+
         return frame
 
 men = Menu()
