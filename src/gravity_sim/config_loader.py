c91469c6211567943830686a2ca5eec67aa8564c
samh366 <samuelhartley47@gmail.com>
2025-09-16 02:40:39 +0100

    Improve colour methods

--- src/gravity_sim/config_loader.py
+++ src/gravity_sim/config_loader.py
@@ -1,6 +1,7 @@
 import random
 import re
 from dataclasses import dataclass, field
+from gravity_sim.vector import Vector
 
 @dataclass
 class Color:
@@ -8,6 +9,34 @@ class Color:
     r: int
     g: int
     b: int
+
+    def __iter__(self):
+        """Iterate through the values in the colour."""
+        return [self.r, self.g, self.b].__iter__()
+
+    def __getitem__(self, key):
+        """Return the requested item."""
+        match key:
+            case 0:
+                return self.r
+            case 1:
+                return self.g
+            case 2:
+                return self.b
+        raise IndexError(f"Color object does not have index {key}!")
+
+    @classmethod
+    def random_colour(cls) -> "Color":
+        """Return a random color object.
+
+        Returns:
+            Color: A color object with random RGB values between 50 and 200.
+        """
+        return cls(
+            r=random.randint(50, 200),
+            g=random.randint(50, 200),
+            b=random.randint(50, 200),
+        )
 
     @classmethod
     def from_iterable(cls, iterable: object) -> "Color":
@@ -19,10 +48,15 @@ class Color:
         Returns:
             Color: A Color object of the provided color.
         """
+        if iterable is None:
+            return cls.random_colour()
+
+        if not hasattr(iterable, "__iter__"):
+            raise ValueError(f"Object {iterable} does not have attribute __iter__.")
 
         def format(num: int):
             """Bound a number between 0 and 255, and round it to an integer."""
-            return min(255, min(0, round(num)))
+            return min(255, max(0, round(num)))
 
         return cls(
             format(iterable[0]),
@@ -30,25 +64,9 @@ class Color:
             format(iterable[2]),
         )
 
-    def as_tuple(self) -> tuple:
-        """Return the color as an RGB tuple."""
-        return (self.r, self.g, self.b)
-
     def __str__(self) -> str:
         """Return a string representation of the color."""
         return str(self.as_tuple())
-
-def random_colour() -> Color:
-    """Return a random color.
-
-    Returns:
-        Color: A color object with random RGB values between 50 and 200.
-    """
-    return Color(
-        r=random.randint(50, 200),
-        g=random.randint(50, 200),
-        b=random.randint(50, 200),
-    )
 
 dict_object = {
     "name": "Earth",
@@ -75,18 +93,22 @@ class Object:
     name: str
     mass: int
     radius: int
-    position: list[float] = field(default_factory=list[float])
-    velocity: list[float] = field(default_factory=list[float])
-    color: Color = field(default_factory=random_colour)
+    position: Vector = field(default_factory=Vector)
+    velocity: Vector = field(default_factory=Vector)
+    color: Color = field(default_factory=Color.random_colour)
     satellites: list["Object"]  = field(default_factory=list)
 
     @classmethod
     def from_dict(cls, data: dict):
-        """Create an object from a dictionary representation."""
-
+        """Return an object from the provided data."""
         return cls(
             name=data["name"],
-            satellites = [cls.from_dict(obj) for obj in data["satellites"]]
+            mass=data["mass"],
+            radius=data["radius"],
+            position=Vector(data["position"]),
+            velocity=Vector(data["velocity"]),
+            color=Color.from_iterable(data.get("color")),
+            satellites=[cls.from_dict(obj) for obj in data.get("satellites", [])]
         )
 
 
