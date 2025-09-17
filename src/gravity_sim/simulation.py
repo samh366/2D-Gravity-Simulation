4e338fc0ad7eaa70850d5cbeb95386e1e76f206f
samh366 <samuelhartley47@gmail.com>
2025-09-17 17:53:27 +0100

    Improve old simulation logic and move to new simulation.py

--- src/gravity_sim/simulation.py
+++ src/gravity_sim/simulation.py
@@ -1,10 +1,13 @@
 from gravity_sim.object import Object
+from gravity_sim.vector import Vector
+import math
 
 class Simulation:
-    def __init__(self, name: str, timestep: int, objects: list[Object], description: str = None):
+    def __init__(self, name: str, timestep: int, objects: list[Object], grav_constant: float = 6.6743e-11, description: str = None):
         self.name = name
         self.timestep = timestep
         self.objects = objects
+        self.grav_constant = grav_constant
         self.description = description
 
         if self.description is None:
@@ -41,11 +44,42 @@ class Simulation:
             description=dictionary.get("description")
         )
 
-    def setup():
-        pass
+    def calculate_forces(self):
+        """Compute the forces between all the objects in the simulation."""
+        # TODO: Improve by only doing half
+        for obj1 in self.objects:
+            for obj2 in self.objects:
+                if obj1 is not obj2:
+                    self.calculate_force_between_objects(obj1, obj2)
 
-    def step():
-        pass
+    def calculate_force_between_objects(self, object1: Object, object2: Object) -> None:
+        """Apply the gravitational force of object 2 on object1.
+
+        Args:
+            object1 (Object): Object to apply the force to.
+            object2 (Object): Object creating the force.
+        """
+        distance = object2.position - object1.position
+        sqrDistance = distance[0] ** 2 + distance[1] ** 2
+        if math.isclose(sqrDistance, 0, rel_tol=1e-7):
+            return
+
+        force = self.grav_constant * object1.mass * object2.mass / sqrDistance
+        theta = math.atan2(distance[1], distance[0]) # Calculate angle the force acts at
+        force_vector = Vector.from_magnitude_theta(magnitude=force, theta=theta)
+
+        object1.add_force(force_vector)
+
+    def move_objects(self) -> None:
+        """Move the objects in the simulation based on the forces acting on them."""
+        for obj in self.objects:
+            obj.step(self.timestep)
+            obj.reset_force()
+
+    def step(self):
+        """Step forward the simulation by one timstep."""
+        self.calculate_forces()
+        self.move_objects()
 
     def is_running() -> bool:
         pass
