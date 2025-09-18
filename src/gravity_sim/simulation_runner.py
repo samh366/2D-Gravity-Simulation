7c8a4764ada7df0cd942b8ce13e7f726d5add21a
samh366 <samuelhartley47@gmail.com>
2025-09-18 21:12:32 +0100

    Add run method to simulation runner

--- src/gravity_sim/simulation_runner.py
+++ src/gravity_sim/simulation_runner.py
@@ -1,9 +1,16 @@
+from gravity_sim.config_loader import ConfigLoader
+from gravity_sim.simulation import Simulation
+from gravity_sim.window import Window
+
 class SimulationRunner:
     """Loads and starts simulations."""
 
-    def __init__(self, config_file: str, gui=True):
-        self.config_file = config_file
-        self.gui = gui
+    def run(self, config_file: str):
+        """Load a simulation from the given config file and display it in a window.
 
-    def run(self):
-        pass
+        Args:
+            config_file (str): The config file to load the simulation's starting state from.
+        """
+        sim = ConfigLoader.load_file(config_file)
+        window = Window(sim)
+        window.run()
