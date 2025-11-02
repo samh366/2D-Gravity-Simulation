bb5ade9eed588f09d3aff91267e8f61176812df4
samh366 <samuelhartley47@gmail.com>
2025-11-02 22:38:56 +0000

    Add object tests

--- tests/test_object.py
+++ tests/test_object.py
@@ -1,9 +1,33 @@
+import pytest
+
 from gravity_sim.object import Object
-from pytest import raises
+from gravity_sim.vector import Vector
 
 
 class TestObject:
     """Tests the Object class."""
+
+    @pytest.fixture
+    def obj(self) -> Object:
+        """Fixture to create an example object."""
+        return Object.from_dict(
+            {
+                "name": "Earth",
+                "mass": 5.972e24,
+                "position": [1000, -1000],
+                "velocity": [10.1, 100],
+                "color": [0, 100, 255],
+                "satellites": [
+                    {
+                        "name": "Moon",
+                        "mass": 5972,
+                        "position": [500, -1000],
+                        "velocity": [-5, 205],
+                        "color": [255, 100, 255],
+                    }
+                ],
+            }
+        )
 
     def test_from_dict_with_satellite(self):
         """An object with a satellite should be returned correctly.
@@ -27,21 +51,56 @@ class TestObject:
                 }
             ],
         }
-        actual_objects = Object.from_dict(dict_object)
-        assert isinstance(actual_objects, list)
-        assert len(actual_objects) == 2
-        earth = actual_objects[0]
-        moon = actual_objects[1]
+        earth = Object.from_dict(dict_object)
+        assert isinstance(earth, Object)
 
         assert earth.name == dict_object["name"]
         assert earth.mass == dict_object["mass"]
-        assert tuple(earth.position) == tuple(dict_object["position"])
-        assert tuple(earth.velocity) == tuple(dict_object["velocity"])
+        assert earth.position.to_tuple() == tuple(dict_object["position"])
+        assert earth.velocity.to_tuple() == tuple(dict_object["velocity"])
         assert tuple(earth.color) == tuple(dict_object["color"])
+
+        assert len(earth.satellites) == 1
+        moon = earth.satellites[0]
 
         satellite_dict = dict_object["satellites"][0]
         assert moon.name == satellite_dict["name"]
         assert moon.mass == satellite_dict["mass"]
-        assert tuple(moon.position) == (1500, -2000)
-        assert tuple(moon.velocity) == (5.1, 305)
+        assert moon.position.to_tuple() == (1500, -2000)
+        assert moon.velocity.to_tuple() == (5.1, 305)
         assert tuple(moon.color) == tuple(satellite_dict["color"])
+
+    def test_add_force(self, obj: Object):
+        """add_force should add a force to the object."""
+        obj.force = Vector()
+        obj.add_force(Vector(2, 5))
+
+        assert obj.force[0] == 2
+        assert obj.force[1] == 5
+
+    def test_reset_force(self, obj: Object):
+        """reset_force should reset an object's force to 0, 0."""
+        obj.force = Vector(2, 5)
+        obj.reset_force()
+
+        assert obj.force[0] == 0
+        assert obj.force[1] == 0
+
+    def test_get_position(self, obj: Object):
+        """get_position should return the correct position."""
+        obj.position = Vector(1, 2)
+        result = obj.get_position()
+
+        assert result[0] == 1
+        assert result[1] == 2
+
+    def test_get_satellites(self, obj: Object):
+        """get_satellites should return a list containing this object and all satelites."""
+        moon = obj.satellites[0]
+
+        satellites = obj.get_satellites()
+
+        assert isinstance(satellites, list)
+        assert len(satellites) == 2
+        assert satellites[0] is obj
+        assert satellites[1] is moon
